3a5e243a7fc36be245dfcc2356ca84497f03459c
Daniel <danigondiaz@gmail.com>
2024-06-11 22:25:08 +0200

    Csrf2

--- backend/settings.py
+++ backend/settings.py
@@ -160,4 +160,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = ['localhost','web-production-4fe56.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-4fe56.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['http://*','https://web-production-4fe56.up.railway.app']
